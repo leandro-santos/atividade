f5f8785b1be779ce3da21dc95427f3e021630979
Leandro <leandrosantos@clickjogos.com.br>
2012-11-07 11:37:53 -0200

    aula 4

---
+++ producer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Producer::Application.config.secret_token = 'dbd733d7dced3c37018ec1503e2543a66d0a7af38113c476013d52a87a407d95455693a42ee18b2d49618895ae4466601aa51cfcd0118e675c2e585562598895'
