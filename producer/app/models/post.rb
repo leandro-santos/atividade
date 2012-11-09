22bf3b4e3bc4dc40960d30cd92a8c4516fa60783
Leandro <leandrosantos@clickjogos.com.br>
2012-11-09 18:01:14 -0200

    Atualização 09/11

--- producer/app/models/post.rb
+++ producer/app/models/post.rb
@@ -1,12 +1,14 @@
-class Post < ActiveRecord::Base
-  attr_accessible :body, :excerpt, :title, :author_id
+    class Post < ActiveRecord::Base
 
-  has_many :comments, :dependent => :destroy
-  has_and_belongs_to_many :categories
- # has_many :posts, foreign_key: "author_id"
-  belongs_to :author, :class_name => "User", :foreign_key => "author_id"
+      attr_accessible :body, :excerpt, :title,
+        :author_id, :category_ids, :slug, :draft
 
-  validates_presence_of :title, :body, :author
-  validates_associated :author
+      validates_presence_of :title, :body, :author, :slug
+      validates_associated :author
+      validates_inclusion_of :draft, in: [true, false]
 
-end
+      has_many :comments, :dependent => :destroy
+      belongs_to :author, class_name: "User", foreign_key: "author_id"
+      has_and_belongs_to_many :categories
+
+    end
